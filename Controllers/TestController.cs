c550c8f7946b5c232acfd948d030b5a5c5b33390
engYasir <yaazir143@gmail.com>
2025-07-19 10:10:28 +0300

    api commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using WebApplication2.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Data.SqlClient;
+
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly DatabaseHelper _dbHelper;
+
+        public TestController(IConfiguration config)
+        {
+            string? connStr = config.GetConnectionString("WBHConnection");
+            if (string.IsNullOrEmpty(connStr))
+                throw new Exception("Connection string 'WBHConnection' is missing.");
+
+            _dbHelper = new DatabaseHelper(connStr);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var items = _dbHelper.GetTestItems();
+            return Ok(items);
+        }
+    }
+}
---
+++ Services/DatabaseHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<Test> GetTestItems()
+        {
+            var items = new List<Test>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT name, quantity FROM test", connection);
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    items.Add(new Test
+                    {
+                        Name = reader["name"].ToString(),
+                        Quantity = reader["quantity"] == DBNull.Value ? 0 : Convert.ToInt32(reader["quantity"])
+                    });
+                }
+            }
+
+            return items;
+        }
+    }
+}
